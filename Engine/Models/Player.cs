d6ff15e7462870519c6cbe59c59bda50b60ac04f
eri_l <eri_l@LAPTOP-J2LRN1EN>
2021-01-03 21:19:53 -0500

    "Engine" folder created. Contains folders: Models & ViewModels
    Models will host classes such as players, monsters, etc.; only players for now.
    
    ViewModels will act as Presenter/Controller/MVP betwen Models and MainWindow

---
+++ Engine/Models/Player.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Engine.Models
+{
+    public class Player
+    {
+        public string Name { get; set;}
+        public string CharacterClass  { get; set;}
+        public int HitPoints  { get; set;}
+        public int ExpPoints  { get; set;}
+        public int Level  { get; set;}
+        public int Gold  { get; set;}
+        
+
+
+    }
+}
---
+++ Engine/ViewModels/GameSession.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+//connects us to Models folder; allows us to look for Player
+using Engine.Models;
+
+//The ViewModels Folder connects Models and the MainWindow;
+
+namespace Engine.ViewModels
+{
+    class GameSession
+    {
+        //Using Player allows us to connect to Players in Models folder
+        Player CurrentPlayer { get; set; }
+
+        //Will be creating character instances in other ways in the future, but for notes sake:
+        public GameSession()
+        {
+            CurrentPlayer = new Player();
+            CurrentPlayer.Name = "Eri";
+            CurrentPlayer.CharacterClass = "UwU";
+            CurrentPlayer.HitPoints = 100;
+            CurrentPlayer.ExpPoints = 0;
+            CurrentPlayer.Gold = 50;
+
+        }
+    }
+}
